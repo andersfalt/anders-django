1910dba81c89cb073ebb0fb11beb0e1a99877d83
andersfalt <andersfalt1@gmail.com>
2021-08-21 16:31:50 +0000

    Initial Commit

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://gekxfeumwdzfbi:d84c14186e1f0976eed798b4bf825080f593264f223bac0ae7c6c22a6c4f5623@ec2-44-195-201-3.compute-1.amazonaws.com:5432/d7o102pvaefr6o')
 }
 
 
